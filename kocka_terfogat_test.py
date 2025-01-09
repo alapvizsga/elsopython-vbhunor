e006969aaad68c0e396d74dfc59a1a52af914853
Englert Ervin  Brunó <englert@users.noreply.github.com>
2025-01-09 22:21:17 +0100

    Update kocka_terfogat_test.py

--- kocka_terfogat_test.py
+++ kocka_terfogat_test.py
@@ -13,17 +13,4 @@ def test_kocka_terfogat():
     assert py1.kocka_terfogat(11) == 1331
     assert py1.kocka_terfogat(12) == 1728
     assert py1.kocka_terfogat(13) == 2197
-    assert py1.kocka_terfogat(14) == 2744
-    assert py1.kocka_terfogat(15) == 3375
-    assert py1.kocka_terfogat(16) == 4096
-    assert py1.kocka_terfogat(17) == 4913
-    assert py1.kocka_terfogat(18) == 5832
-    assert py1.kocka_terfogat(19) == 6859
-    assert py1.kocka_terfogat(20) == 8000
-    assert py1.kocka_terfogat(21) == 9261
-    assert py1.kocka_terfogat(22) == 10648
-    assert py1.kocka_terfogat(23) == 12167
-    assert py1.kocka_terfogat(24) == 13824
-    assert py1.kocka_terfogat(25) == 15625
-    assert py1.kocka_terfogat(26) == 17576
-   
+    
